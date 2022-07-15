696ddb63e3feb6190aff57ef2c2aeb23151d737c
GK <kg@GKs-MacBook-Pro.local>
2022-07-15 20:29:48 +0200

    complete basic app

--- check_redis.py
+++ check_redis.py
@@ -2,7 +2,8 @@ import os
 import redis
 import time
 import streamlit as st
-
+import plotly.express as px
+import pandas as pd
 
 def connect_redis():
     redis_connect = None
@@ -23,15 +24,41 @@ def connect_redis():
                 print("Redis connect failed")
     return redis_connect
 
+def show_all_wallets_donut(current):
+    fig = px.pie(
+    hole = 0.3,
+    names = current.keys(),
+    values = current.values(),
+)
+    st.plotly_chart(fig)
+
+def show_total_numbers(total, current):
+    st.title(f"Total: {total:,}")
+    st.subheader(f"Wallets: {len(current)}")
+
 
 r = connect_redis()
 machines = r.keys()
 total = 0
 current = {}
-for machine in sorted(machines):
-    value = int(r.get(machine))
-    current[machine.decode("utf-8")] = value
-    print(f"{machine.decode('utf-8').strip('.local')}: {value:,}")
-    total += value
+st.title("ABL")
+
+data_load_state = st.text("Loading data")
+bar = st.progress(0)
+for i, machine in enumerate(sorted(machines)):
+    bar.progress((i+1)/len(machines))
+    curr_key = machine.decode("utf-8").split(".")[0]
+    curr_value = int(r.get(machine))
+    current[curr_key] = curr_value
+    print(f"{curr_key}: {curr_value:,}")
+    total += curr_value
+st.spinner(" ")
+
 print(f"{total:,}")
 print(current)
+
+show_total_numbers(total, current)
+# show_all_wallets_donut(current)
+st.write(current)
+
+time.sleep(10)
