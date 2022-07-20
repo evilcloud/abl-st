36028e92c1db0fb3c999d5babc81a7e79a9399f6
GK <kg@GKs-MacBook-Pro.local>
2022-07-20 09:36:21 +0200

    intoduced safe machines (tested)

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+from calendar import c
 import streamlit as st
 import pandas as pd
 import mongo_data
 import time
+from st_aggrid import AgGrid
 
-# from st_aggrid import AgGrid
 
-wallets, workers, found_machines = mongo_data.data_wallets_workers()
+wallets, workers, found_machines, unsafe_machines = mongo_data.data_wallets_workers()
 
 df_wallets = pd.DataFrame(wallets)
 df_workers = pd.DataFrame(workers)
@@ -32,6 +33,9 @@ st.title("ABEL")
 
 st.write(f"last update: {time.strftime('%Y-%m-%d %H:%M:%S')}")
 
+
+refresh = st.button("Refresh now")
+
 st.title(f"Total:")
 st.markdown(f'<p class="big-font">{total:,}</p>', unsafe_allow_html=True)
 st.write(f"###### Total machines: {total_machines}")
@@ -40,13 +44,18 @@ if found_machines:
     st.warning(f"Found {len(found_machines)} lost machines. Total may be inaccurate!")
     st.table(found_machines)
 
-col1, col2 = st.columns(2)
-with col1:
-    st.subheader(f"Wallets {df_wallets_count}")
+if unsafe_machines:
+    st.warning(f"Found {len(unsafe_machines)} unsafe machines. Not included in Total!")
+    st.table(unsafe_machines)
 
-    st.dataframe(df_wallets)
+# col1, col2 = st.columns(2)
+# with col1:
+st.subheader(f"Wallets {df_wallets_count}")
 
+# st.dataframe(df_wallets)
+AgGrid(df_wallets)
 
-with col2:
-    st.subheader(f"Workers {df_workers_count}")
-    st.dataframe(df_workers)
+# with col2:
+st.subheader(f"Workers {df_workers_count}")
+# st.dataframe(df_workers)
+AgGrid(df_workers)
--- mongo_data.py
+++ mongo_data.py
@@ -2,10 +2,17 @@ from functools import total_ordering
 import os
 import sys
 import datetime
+from git import safe_decode
 import humanize
 from numpy import block
 import pymongo
-import json
+
+
+def load_file(filename):
+    if os.path.exists(filename):
+        with open(filename, "r") as f:
+            lines = f.readlines()
+    return [x.strip() for x in lines]
 
 
 def connect_mongodb():
@@ -29,55 +36,58 @@ def get_mongo_data():
 def load_lost_machines():
     lost_machines = []
     lost_machines_file = "lost_machines.txt"
-    if os.path.exists(lost_machines_file):
-        with open(lost_machines_file, "r") as f:
-            lost_machines = list(f.readlines())
-    lost_machines = [x.strip() for x in lost_machines]
+    lost_machines = load_file(lost_machines_file)
     return lost_machines
+
+
+def load_safe_machines():
+    safe_machines = []
+    safe_machines_file = "safe_machines.txt"
+    safe_machines = load_file(safe_machines_file)
+    return safe_machines
 
 
 def process_df(data):
     wallets = []
     workers = []
     found_machines = []
+    unsafe_machines = []
     lost_machines = load_lost_machines()
+    safe_machines = load_safe_machines()
     for item in data:
+        machine = item["_id"]
         current_time = datetime.datetime.utcnow()
         last_update = item.get("update_time", None)
-        # block_height = str(item.get("block_height", " "))
-        version = item.get("version", " ")
         timedelta = (
             humanize.naturaldelta(current_time - last_update) if last_update else ""
         )
+        # block_height = str(item.get("block_height", " "))
+        version = item.get("version", " ")
         is_worker = item.get("cluster", None)
+        entry = {
+            "Machine": machine,
+            "Balance": item["total_balance"],
+            "Programmatic": item.get("programmatic"),
+            "Since last update": str(timedelta),
+            "Version": version,
+        }
         if is_worker or is_worker == None:
-            workers.append(
-                {
-                    "Machine": item["_id"],
-                    "Since last update": str(timedelta),
-                    "Version": version,
-                }
-            )
+            workers.append(entry)
         else:
-            wallets.append(
-                {
-                    "Machine": item["_id"],
-                    "Balance": item["total_balance"],
-                    "Programmatic": item["programmatic"],
-                    "Since last update": str(timedelta),
-                    "Version": version,
-                }
-            )
-        if item["_id"] in lost_machines and item["programmatic"]:
-            found_machines.append(item["_id"])
+            if machine in safe_machines:
+                wallets.append(entry)
+            else:
+                unsafe_machines.append(entry)
+        if machine in lost_machines and item["programmatic"]:
+            found_machines.append(machine)
 
-    return wallets, workers, found_machines
+    return wallets, workers, found_machines, unsafe_machines
 
 
 def data_wallets_workers():
     data = get_mongo_data()
-    wallets, workers, found_machines = process_df(data)
-    return wallets, workers, found_machines
+    wallets, workers, found_machines, unsafe_machines = process_df(data)
+    return wallets, workers, found_machines, unsafe_machines
 
 
 if __name__ == "__main__":
