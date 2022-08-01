ecb6c27f25e80315e3d8cd0673c8e81ce7a5b6d9
GK <kg@GKs-MacBook-Pro.local>
2022-08-01 15:09:40 +0800

    rewritten app for api access and minimal inplace processing

--- processor.py
+++ processor.py
@@ -4,6 +4,7 @@ import datetime
 import humanize
 from numpy import block
 import deta_data
+from dataclasses import dataclass
 
 
 def load_file(filename):
@@ -27,31 +28,82 @@ def load_safe_machines():
     return safe_machines
 
 
-def get_wallets():
-    wallets = deta_data.Detadb("ABEL_MINING")
-    # lost_machines = load_lost_machines()
-    # lost = []
-    # for item in wallets:
-    #     if item("key") in lost_machines:
-    #         lost.append(item)
-    #         wallets.remove(item)
-    return wallets
+def call_db_list(db_name):
+    db = deta_data.Detadb(db_name)
+    ret = db.entries_obj.items
+    return ret
 
 
-def get_ping():
-    ping = deta_data.Detadb("ABEL_PING")
+def get_wallets(wallet_db_name):
+    """
+    Get all wallets from Deta, match against 'lost wallets' list and return the wallets present,
+    wallets that deemed to have been lost, but now found,
+    and wallets that are deemed to be safe but missing in the current query
+    """
+    # wallets = deta_data.Detadb(wallet_db_name)
+    # unidentified_wallets = wallets.entries.items
+    unidentified_wallets = call_db_list(wallet_db_name)
+    lost_wallets = load_lost_machines()
+    safe_wallets = load_safe_machines()
+    recovered_wallets = []
+    current_safe_wallets = []
+    for item in unidentified_wallets:
+        print(item["key"], "... ", end="")
+        if item["key"] in lost_wallets:
+            print("lost wallet")
+            recovered_wallets.append(item)
+            unidentified_wallets.remove(item)
+        elif item["key"] in safe_wallets:
+            print("safe wallet")
+            current_safe_wallets.append(item)
+            unidentified_wallets.remove(item)
+        else:
+            print("unknown")
+    return current_safe_wallets, recovered_wallets, unidentified_wallets
+
+
+def get_ping_machines(ping_db_name):
+    ping = call_db_list(ping_db_name)
     return ping
 
 
-def add_time_diff(data):
+def replace_updatetime_updatediff(data):
     for item in data:
         current_time = datetime.datetime.utcnow()
         last_update = item.get("updatetime", None)
-        timedelta = (
-            humanize.naturaldelta(current_time - last_update) if last_update else ""
-        )
+        if last_update:
+            last_update = datetime.datetime.strptime(last_update, "%Y-%m-%dT%H:%M:%S")
+            timedelta = humanize.naturaldelta(current_time - last_update)
+        else:
+            timedelta = None
         item["since last update"] = timedelta
+        del item["updatetime"]
     return data
+
+
+def clean_wrong_update_amount(data):
+    for item in data:
+        update_amount = item['update amount']
+        if item["balance"] == update_amount:
+            update_amount = " "
+    return data
+
+
+def annull_if_equal(data, base, match):
+    for item in data:
+        if item[base] == item[match]:
+            item[match] = " "
+    return data
+
+
+def clean_wallets(data):
+    a = annull_if_equal(data, "balance", "update amount")
+    b = annull_if_equal(a, "block", "update block difference")
+    return annull_if_equal(b, "block", "update block difference")
+
+
+def clean_ping(data):
+    return annull_if_equal(data, "block", "blocks since last")
 
 
 def process_df(data):
@@ -91,11 +143,6 @@ def process_df(data):
 
     return wallets, workers, found_machines, unsafe_machines
 
-
-def data_wallets_workers():
-    data = get_mongo_data()
-    wallets, workers, found_machines, unsafe_machines = process_df(data)
-    return wallets, workers, found_machines, unsafe_machines
 
 
 if __name__ == "__main__":
